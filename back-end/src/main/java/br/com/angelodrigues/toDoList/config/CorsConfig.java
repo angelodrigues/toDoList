2a793f57c70cbd94a3f86852dceb3358574648fb
Angelo Barcelos <angelorodrigues318@gmail.com>
2023-10-17 22:35:10 -0300

    Fix the Cors Error

---
+++ back-end/src/main/java/br/com/angelodrigues/toDoList/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package br.com.angelodrigues.toDoList.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(false);
+        config.setAllowedOrigins(Arrays.asList("*"));
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+}
--- front-end/src/pages/login/Login.tsx
+++ front-end/src/pages/login/Login.tsx
@@ -1,26 +1,68 @@
 import './Login.css';
+import { useState } from 'react';
+import axios from 'axios';
 
-export function Login(){
-    return(
-        <main>
-            <header>
-                <h2>To Do List</h2>
-            </header>
-            <div className="content">
-                <div className="inputData">
-                    <label htmlFor="name">Nome</label>
-                    <input type="text" name="name" id="name" />
-                    <label htmlFor="password">Password</label>
-                    <input type="text" name="password" id="password" />
-                </div>
-                <div className="register">
-                    <a href="http://" target="_blank" rel="noopener noreferrer">New Register</a>
-                </div>
-                <div className="connectButton">
-                    <button>Go to tasks !</button>
-                </div>
-            </div>
-            <footer></footer>
-        </main>
-    );
+export function Login() {
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      // Create a base64-encoded string of "name:password"
+      const authString = btoa(`${name}:${password}`);
+
+      // Create headers with the Basic Authentication credentials
+      const headers = {
+        Authorization: `Basic ${authString}`,
+      };
+
+      // Make an Axios GET request to your API
+      const response = await axios.get('http://localhost:8080/tasks/', {
+        headers: headers,
+      });
+
+      // Handle the response here, e.g., update your state or navigate to another page
+      console.log('Response data:', response.data);
+    } catch (error) {
+      // Handle any errors that may occur during the request
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <main>
+      <header>
+        <h2>To Do List</h2>
+      </header>
+      <div className="content">
+        <div className="inputData">
+          <label htmlFor="name">Nome</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password" // Use type="password" to hide the password
+            name="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="register">
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            New Register
+          </a>
+        </div>
+        <div className="connectButton">
+          <button onClick={handleLogin}>Go to tasks !</button>
+        </div>
+      </div>
+      <footer></footer>
+    </main>
+  );
 }
