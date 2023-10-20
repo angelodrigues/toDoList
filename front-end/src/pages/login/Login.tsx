943d12f9bd1de8d8fc533d81c5ad4e460470087a
Angelo Barcelos <angelorodrigues318@gmail.com>
2023-10-20 07:09:49 -0300

    Converting components to modal

--- front-end/src/pages/login/Login.tsx
+++ front-end/src/pages/login/Login.tsx
@@ -2,6 +2,18 @@ import './Login.css';
 import { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import Modal from 'react-modal';
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',    
+  },
+};
 
 type Headers = {
   Authorization: string;
@@ -14,6 +26,17 @@ type LoginProps = {
 export function Login({ setLoginHeaders }: LoginProps) {
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
+  const [userName, setUserName] = useState('');
+
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
 
   const handleLogin = async () => {
     try {
@@ -53,11 +76,53 @@ export function Login({ setLoginHeaders }: LoginProps) {
           id="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
+        /> 
+       </div>
       <div className="register">
-        <Link to="../register">New Register</Link>
+        <a href="#" onClick={openModal}>New Register</a>
+        {/* <Link to="../register">New Register</Link> */}
       </div>
+      {/* <div className="modal">
+        <button onClick={openModal}>Open</button>
+      </div> */}
+      {modalIsOpen && <div className="dark-backdrop"></div>}
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
+      >
+              <div className="inputData">
+                <label htmlFor="name">Nome</label>
+                <input
+                    type="text"
+                    name="name"
+                    id="name"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <label htmlFor="userName">User Name</label>
+                <input
+                    type="text"
+                    name="userName"
+                    id="userName"
+                    value={userName}
+                    onChange={(e) => setUserName(e.target.value)}
+                />
+                <label htmlFor="password">Password</label>
+                <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+            </div>
+            <div className="connectButton">
+                <button onClick={handleLogin}>Register</button>
+            </div>
+      </Modal>
       <div className="connectButton">
         <Link to="../tasks"><button onClick={handleLogin}>Go to tasks !</button></Link>
       </div>
